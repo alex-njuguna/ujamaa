ce4b1faa91264c95d016964a824026ac7ef38440
= <=>
2023-06-27 23:10:41 +0300

    signin & authentication functionality

--- ujamaa/core/urls.py
+++ ujamaa/core/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+
     path('signup/', signup, name='signup'),
+    path('signin/', signin, name='signin'),
+    path('logout/', logout, name='logout'),
 ]
--- ujamaa/core/views.py
+++ ujamaa/core/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import Profile
 
+
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
 def signup(request):
-
     if request.method == "POST":
         username = request.POST['username']
         email = request.POST['email']
@@ -41,3 +43,25 @@ def signup(request):
             return redirect('signup')
     else:
         return render(request, 'signup.html')
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, 
+                                 password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Incorrect username or password')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
