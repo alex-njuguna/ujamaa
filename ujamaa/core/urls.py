695ecb6a0a6becc113c2f2fddbf273770cd56a91
= <=>
2023-06-27 03:39:42 +0300

    signup phase 1

--- ujamaa/core/urls.py
+++ ujamaa/core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('signup/', signup, name='signup'),
 ]
--- ujamaa/core/views.py
+++ ujamaa/core/views.py
@@ -1,4 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
 
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email not available')
+                return redirect(request, 'signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Invalid username')
+                return redirect(request, 'signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password1)
+                user.save()
+        else:
+            messages.info(request, 'Passwords not matching')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
