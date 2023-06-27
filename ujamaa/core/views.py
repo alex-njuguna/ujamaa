13b4da75c1090b8af26d010a70fb5c04208db4f0
= <=>
2023-06-27 22:21:38 +0300

    signup phase 2

--- ujamaa/core/views.py
+++ ujamaa/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
+from .models import Profile
 
 def index(request):
     return render(request, 'index.html')
@@ -17,13 +18,24 @@ def signup(request):
         if password1 == password2:
             if User.objects.filter(email=email).exists():
                 messages.info(request, 'Email not available')
-                return redirect(request, 'signup')
+                return redirect('signup')
             elif User.objects.filter(username=username).exists():
                 messages.info(request, 'Invalid username')
-                return redirect(request, 'signup')
+                return redirect('signup')
             else:
-                user = User.objects.create_user(username=username, email=email, password=password1)
+                user = User.objects.create_user(
+                    username=username, email=email, 
+                    password=password1)
                 user.save()
+
+                # log user in and direct to settings page
+
+                # create a profile object for new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(
+                    user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
         else:
             messages.info(request, 'Passwords not matching')
             return redirect('signup')
