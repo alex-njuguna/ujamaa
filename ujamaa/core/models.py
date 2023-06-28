63826ad2851abd612fe01b8d91390f98f9cb31ca
= <=>
2023-06-29 00:26:32 +0300

    posts model

--- ujamaa/core/models.py
+++ ujamaa/core/models.py
@@ -1,15 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
+import uuid
 
-"""
-create model profile:
-    user - link to django auth model
-    user id - int field
-    bio - text field
-    image - save in media dir(profile_images dir)
-            default img(img filed)
-    location - char field  
-"""
+
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
@@ -19,3 +13,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
--- ujamaa/core/urls.py
+++ ujamaa/core/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('settings/', settings, name='settings'),
+    path('upload/', upload, name='upload'),
 
     path('signup/', signup, name='signup'),
     path('signin/', signin, name='signin'),
